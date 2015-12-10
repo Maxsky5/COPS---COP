983248110f414c68337f5d4e841420165d75efd1
= <thomaspeyrou@gmail.com>
2015-12-10 16:55:09 +0100

    Add config to db

--- dao/ConnectDb.py
+++ dao/ConnectDb.py
@@ -1,37 +1,38 @@
 # -*-coding: utf-8-*-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker
+import ConfigParser
 
 
-class ConnectDb:
+class ConnectDb(object):
     Base = declarative_base()
-    session = sessionmaker()
+    engine = None
+    Session = None
     host = None
     db_name = None
     username = None
     password = None
     port = None
 
-    def __init__(self, host, db_name, username, password, port=""):
-        self.host = host
-        self.db_name = db_name
-        self.username = username
-        self.password = password
-        self.port = port
-        self.engine = create_engine(
-            'mysql://'+self.username +
-            ':'+self.password +
-            '@'+self.host +
-            ':'+self.port +
-            '/'+self.db_name,
-            echo=True
-        )
-        self.Base = declarative_base(self.engine)
+    @staticmethod
+    def session():
+        if ConnectDb.engine is None:
+            config = ConfigParser.ConfigParser()
+            config.read("../config.ini")
+            ConnectDb.host = config.get('database', 'host')
+            ConnectDb.db_name = config.get('database', 'dbname')
+            ConnectDb.username = config.get('database', 'username')
+            ConnectDb.password = config.get('database', 'password')
+            ConnectDb.port = config.get('database', 'port')
+            ConnectDb.engine = create_engine(
+                'mysql://'+ConnectDb.username +
+                ':'+ConnectDb.password +
+                '@'+ConnectDb.host +
+                ':'+ConnectDb.port +
+                '/'+ConnectDb.db_name,
+                echo=True
+            )
+            ConnectDb.Session = sessionmaker(bind=ConnectDb.engine)
 
-    def session(self):
-        return scoped_session(sessionmaker(bind=self.engine))
-
-
-
-
+        return ConnectDb.Session()
