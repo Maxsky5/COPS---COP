df6247143df87008000cafc25e4709d76dcc9e8f
Twur <arthur.sore@gmail.com>
2016-01-12 13:45:31 +0100

    led manager fix

--- utils/LedManager.py
+++ utils/LedManager.py
@@ -8,35 +8,35 @@ class LedManager:
 
     def __init__(self):
         # set modes (out)
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(7, GPIO.OUT) # red
-        GPIO.setup(13, GPIO.OUT) # yellow
-        GPIO.setup(11, GPIO.OUT) # green
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.OUT) # red
+        GPIO.setup(27, GPIO.OUT) # yellow
+        GPIO.setup(17, GPIO.OUT) # green
 
 
     def blinkOK(self):
         # QR is OK
         times = 4
         for i in range(0,times):
-            GPIO.output(11,True)
+            GPIO.output(17,True)
             time.sleep(0.5)
-            GPIO.output(11,False)
+            GPIO.output(17,False)
             time.sleep(0.5)
 
     def blinkKO(self):
         # QR is not OK
         times = 4
         for i in range(0,times):
-            GPIO.output(7,True)
+            GPIO.output(4,True)
             time.sleep(0.5)
-            GPIO.output(7,False)
+            GPIO.output(4,False)
             time.sleep(0.5)
 
     def blinkSyncProblem(self):
         # sync Problem
         times = 9
         for i in range(0,times):
-            GPIO.output(13,True)
+            GPIO.output(27,True)
             time.sleep(2)
-            GPIO.output(13,False)
+            GPIO.output(27,False)
             time.sleep(2)
